0027ac81f009ef24d7b75056cc20e9007930abe4
Huzaifa Mehmood <154912388+huzaifa782@users.noreply.github.com>
2024-09-20 10:23:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:',
 if ingredients_list:
     ingredients_string = ''
 
-    for chosen_fruits in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
