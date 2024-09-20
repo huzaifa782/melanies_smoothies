a6c5ad41b1fcdbca9720e648a83949e30a371f2b
Huzaifa Mehmood <154912388+huzaifa782@users.noreply.github.com>
2024-09-20 10:40:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -13,7 +13,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 title = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", title)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
@@ -38,16 +39,15 @@ if Ingredients_list:
     for fruit_chosen in Ingredients_list:
             ingredients_string += fruit_chosen + ' '
 
-            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+           
         
             st.subheader(fruit_chosen + ' Nutrition Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER) 
-            values ('""" + ingredients_string + "'"+',' + "'"+ title + "'"+ ');'
+       my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
     
